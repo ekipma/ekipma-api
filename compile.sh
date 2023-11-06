370550cb770766dc8ab4c23879c7de4aa3db03b7
hayyaun <hayyanhami@outlook.com>
2023-11-06 06:21:24 +0330

    enhance dart package

--- compile.sh
+++ compile.sh
@@ -3,5 +3,5 @@
 protoc protos/*.proto \
 --go_out=./ --go_opt=paths=import \
 --go-grpc_out=./ --go-grpc_opt=paths=import \
---dart_out=grpc:dart \
+--dart_out=grpc:dart/lib \
 --proto_path=.
---
+++ dart/lib/protos/ekipma_api.dart
@@ -0,0 +1,7 @@
+library ekipma_api;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
