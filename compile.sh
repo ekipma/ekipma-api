d9b78f4ce17a5e66921f877b5a5e69d1845ade7e
hayyaun <hayyanhami@outlook.com>
2023-11-15 17:12:29 +0330

    enhance sh file

--- compile.sh
+++ compile.sh
@@ -5,3 +5,25 @@ protoc protos/*.proto \
 --go-grpc_out=./ --go-grpc_opt=paths=import \
 --dart_out=grpc:dart/lib \
 --proto_path=.
+
+NVER="0.0.0"
+
+if [ -z "$1" ]
+then
+    echo "No commit message supplied"
+    return 1
+fi
+
+git add .
+git commit -m"$1"
+
+if [ -z "$2" ]
+then
+    echo "No tag name supplied"
+    git tag $NVER
+else
+    git tag $NVER -m"$2"
+fi
+
+git push origin main
+git push origin $NVER
